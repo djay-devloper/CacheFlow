4435c4fc6970507ce8d63e9f6caf21a3aa0adedb
Dhananjay Yadav <djay.developer@gmail.com>
2026-08-15 13:18:30 +0530

    image url localhost to render

--- BACKEND/routes/authRoutes.js
+++ BACKEND/routes/authRoutes.js
@@ -24,7 +24,8 @@ router.post('/login', loginUser);
            return res.status(400).json({ message: 'No file uploaded' });
      }
 
-     const imageUrl = `${req.protocol}://${req.get('host')}/uploads/${req.file.filename}`;
+     const baseUrl = process.env.PUBLIC_BASE_URL || `${req.protocol}://${req.get('host')}`;
+     const imageUrl = `${baseUrl}/uploads/${req.file.filename}`;
      res.status(200).json({ imageUrl });
  });
 
